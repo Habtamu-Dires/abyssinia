1e8a96fdd5d35a0ebb806efc643f86145d92ccd2
Habtamu-Dires <habtamuadugna.d@gmail.com>
2022-08-28 19:27:34 +0300

    progressBar-icon

--- script/script.js
+++ script/script.js
@@ -1,10 +1,4 @@
 
-//scroll animation
-$(document).ready(function() {
-    new WOW().init();
-  });
-
-
 //carousel Animation
 (function($) {
     //Function to animate slider captions
@@ -40,5 +34,39 @@ $(document).ready(function() {
   })(jQuery);
 
 
-  //multi-step form code
-  
+//multi-step form code
+  let currentTab = 0; //current tap set to be the first tab
+  showTab(currentTab);
+
+function showTab(n) {
+    //this function will display the specified tab of the form ...
+    let x = document.getElementsByClassName("multi-form-tab");
+    x[n].style.display = "block";
+}
+//function to mark progress bar 
+//add active class to bar element
+function setProgressBar(n, status){
+   if(status) 
+      $("#progressbar li").eq(n).addClass("active");
+   else {
+      $("#progressbar li").eq(n+1).removeClass("active");
+   }
+}
+
+function nextPrev(n) {
+    //This functin will figure out which tab to display
+    let x = document.getElementsByClassName("multi-form-tab");
+
+    //Hide the current tab
+    x[currentTab].style.display = "none";
+    //Increase or decrease the current tab by 1
+    currentTab = currentTab + n;
+
+    //otherwise, display the correct tab
+    showTab(currentTab);
+    //set progress bar
+    if(n===1) 
+      setProgressBar(currentTab, true);
+    else 
+      setProgressBar(currentTab, false);
+}
